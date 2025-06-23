2a32ed31e41d7aac0b67bd3ccf707dfbb9adac2c
Brian <okellobrian18@gmail.com>
2025-06-23 17:42:51 +0300

    simple get episodes route

---
+++ server/controllers/guest_controller.py
@@ -0,0 +1,11 @@
+
+from flask import Blueprint, jsonify
+from server.models.guest import Guest
+
+guest_bp = Blueprint('guest', __name__)
+
+@guest_bp.route('/guests', methods=['GET'])
+def get_guests():
+    guests = Guest.query.all()
+    guest_list = [guest.to_dict() for guest in guests]
+    return jsonify(guest_list), 200
