f5402d0c7e3adfb2763d56c4768939f1e9139893
Brian <okellobrian18@gmail.com>
2025-06-23 20:01:09 +0300

    Configuring JWT with the app and changing data type to enable password length

--- server/app.py
+++ server/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager 
 from server.models import db
 from server.config import Config
 from server.controllers import all_blueprints
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+jwt = JWTManager(app)
 
 db.init_app(app)
 migration = Migrate(app, db)
--- server/config.py
+++ server/config.py
@@ -1,5 +1,12 @@
 # server/config.py
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:0Picklerick.@localhost:5432/late-night-show'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')  
+
